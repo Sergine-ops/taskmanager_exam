3d70b3e7e3b87c6c7b84060cd3b02eed480dde25
Sergine-ops <156674288+Sergine-ops@users.noreply.github.com>
2024-06-27 17:35:48 +0200

    initial commit

--- api/index.js
+++ api/index.js
@@ -7,23 +7,21 @@ const debug = require("debug")("api");
 const taskRoutes = require("./routes/tasks");
 const { connectDb } = require("./config/connection");
 
-// LOAD ENV VARS
+
 dotenv.config();
 
 async function main() {
   debug("booting %o", "App");
 
-  // Application
-  const app = express();
 
-  const options = {};
+  const app = express();
 
   app.use(bodyParser.json());
   app.use(cors({ origin: "*" }));
   app.use("/api/v1/tasks", taskRoutes);
 
   app.listen(process.env.PORT, () => {
-    console.log("🚀 server started running on port 4000");
+    console.log("server started running on port 4000");
   });
 }
 
